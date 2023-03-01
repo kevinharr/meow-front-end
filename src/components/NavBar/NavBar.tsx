926845cf3603a2e342442ea1201bd4f5328051d6
kevinharr <kevinharr@verizon.net>
2023-02-28 22:55:39 -0500

    Revert "fix logo"
    This reverts commit 9ca60a53cfea35959fa7da9df09133130e9b6c72.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,6 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
-// assets
-import logo from '../../assets/logo.svg'
-
 // types
 import { User } from '../../types/models'
 
@@ -17,19 +14,17 @@ const NavBar = (props: NavBarProps): JSX.Element => {
   
   return (
     <nav>
-      <NavLink to="/">
-        <img src={logo} alt="Meow Meow Bean" />
-      </NavLink>
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">PROFILES</NavLink></li>
+          <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="/change-password">Change Password</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
         </ul>
       :
         <ul>
-          <li><NavLink to="/login">LOGIN</NavLink></li>
-          <li><NavLink to="/signup">SIGN UP</NavLink></li>
+          <li><NavLink to="/login">Log In</NavLink></li>
+          <li><NavLink to="/signup">Sign Up</NavLink></li>
         </ul>
       }
     </nav>
