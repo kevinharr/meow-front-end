9d1af58e9d5bf15a435e2d6bcb3e816fbc424a3d
kevinharr <kevinharr@verizon.net>
2023-02-28 22:01:19 -0500

    add submitting vote capability

--- src/components/VoteManager/VoteManager.tsx
+++ src/components/VoteManager/VoteManager.tsx
@@ -4,13 +4,15 @@ import noBean from '../../assets/icons/noBean.png'
 
 // types
 import { Profile } from '../../types/models'
+import { VoteManagerFormData } from '../../types/forms'
 
 interface VoteManagerProps {
 	profile: Profile;
+  handleVote: (formData: VoteManagerFormData) => void;
 }
 
 const VoteManager = (props: VoteManagerProps): JSX.Element => {
-	const { profile } = props
+	const { profile, handleVote } = props
 
   const ratingOptions: [ 1, 2, 3, 4, 5 ] = [ 1, 2, 3, 4, 5 ]
   const voteCount = profile.votesReceived.length
@@ -19,17 +21,22 @@ const VoteManager = (props: VoteManagerProps): JSX.Element => {
   profile.votesReceived.forEach(vote => voteSum += vote.value)
 
   const profileRating = voteCount ? voteSum / voteCount : 1
+
+	const handleClick = (evt: React.MouseEvent<HTMLImageElement>): void => {
+    const newValue = parseInt(evt.currentTarget.id)
+    handleVote({ value: newValue, profileId: profile.id })
+  }
   
-  return (
+	return (
     <section>
       {ratingOptions.map((rating): JSX.Element => (
         <img
-				id={rating.toString()}
-				key={rating}
-				src={rating <= profileRating ? bean : noBean} 
-				alt="Bean Symbol"
-				/>
-			
+          id={rating.toString()}
+          key={rating}
+          onClick={handleClick}
+          src={rating <= profileRating ? bean : noBean} 
+          alt="Bean Symbol"
+        />
       ))}
     </section>
   )
