2fecd49807ab30ad91a649d18aade479efc26cba
kevinharr <kevinharr@verizon.net>
2023-02-28 21:09:06 -0500

    create display vote functionality

--- src/types/models.ts
+++ src/types/models.ts
@@ -4,19 +4,20 @@
 
 /* ---------===== auth models =====--------- */
 
+export interface Vote {
+  id: number;
+  value: number;
+  profileId: number;
+  voterId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface Profile {
   name: string;
   photo?: string;
   id: number;
   createdAt: string;
   updatedAt: string;
-}
-
-export interface User {
-  name: string;
-  email: string;
-  profile: { id: number };
-  id: number;
-  createdAt: string;
-  updatedAt: string;
+  votesReceived: Vote[];
 }
